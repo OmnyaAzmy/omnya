44f3d3c606de2013cb69c7ea110ae07c9b208358
omnya azmy <116688348+OmnyaAzmy@users.noreply.github.com>
2023-05-31 17:38:17 +0300

    Add files via upload

---
+++ resources/views/Mails/CustomerMail.blade.php
@@ -0,0 +1,22 @@
+<h1> Order Details </h1> <br><br>
+Dear {{$name}},
+
+Thank you for your recent purchase! We wanted to confirm the details of your order:<br><br>
+
+Email: {{$email}}    <br>
+Address: {{$address}} <br>
+Phone: {{$phone_number}} <br>
+Description: {{$description}} <br>
+Product Price: {{$productprice}} <br>
+Delivery Time: {{$deliverytime}} <br><br>
+If you have any questions or concerns, please do not hesitate to contact us.<br>
+
+Thank you for your business!<br>
+
+Best regards.<br>
+
+Company Name: Home Bazzar.
+
+
+
+
---
+++ resources/views/Mails/ordertofinish.blade.php
@@ -0,0 +1,16 @@
+<h1> Order Details </h1> <br><br>
+
+You have got an email from: {{$name}}  <br><br>
+
+User Details: <br><br>
+
+Name:           {{$name}}        <br>
+Product id:     {{$product_id}}        <br>
+Email:          {{$email}}       <br>
+Address:        {{$address}}       <br>
+Phone:          {{$phone_number}}     <br>
+Description:    {{$description}}     <br>
+
+Thanks.
+
+
